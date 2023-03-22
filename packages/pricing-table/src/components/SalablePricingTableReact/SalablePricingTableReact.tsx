1cfff9d376df8388fe9867a7e00975a3a59b2a65
Coner Murphy <cmurphy@adaptavist.com>
2023-03-22 17:38:44 +0000

    chore: aligned linting config to central config

--- packages/pricing-table/src/components/SalablePricingTableReact/SalablePricingTableReact.tsx
+++ packages/pricing-table/src/components/SalablePricingTableReact/SalablePricingTableReact.tsx
@@ -2,10 +2,7 @@ import { FC, useEffect } from 'react';
 import { IPricingTable, IPricingTableReact } from './types';
 import { JS_SDK_VERSION } from '../../../../constants';
 
-export const SalablePricingTableReact: FC<IPricingTableReact> = ({
-  envConfig,
-  checkoutConfig,
-}) => {
+export const SalablePricingTableReact: FC<IPricingTableReact> = ({ envConfig, checkoutConfig }) => {
   let pricingTable: IPricingTable = null;
 
   useEffect(() => {
@@ -13,26 +10,19 @@ export const SalablePricingTableReact: FC<IPricingTableReact> = ({
       const script = document.createElement('script');
 
       script.src = `https://cdn.salable.${
-        !envConfig.environment || envConfig.environment === 'prod'
-          ? 'app'
-          : 'org'
+        !envConfig.environment || envConfig.environment === 'prod' ? 'app' : 'org'
       }/v${JS_SDK_VERSION}/index.js`;
       script.type = 'module';
       script.id = 'salableCdnScript';
 
       document.body.appendChild(script);
 
-      envConfig.pricingTableNode = document.querySelector(
-        '.salable-pricing-table-react'
-      );
+      envConfig.pricingTableNode = document.querySelector('.salable-pricing-table-react');
 
       script.addEventListener('load', () => {
         if (window.SalablePricingTable) {
           (async () => {
-            pricingTable = new window.SalablePricingTable(
-              envConfig,
-              checkoutConfig
-            );
+            pricingTable = new window.SalablePricingTable(envConfig, checkoutConfig);
             await pricingTable?.init();
           })().catch((error) => {
             // eslint-disable-next-line no-console
